6259b5594b27eff5f5e91bba69d16859e8a4bc93
Stanny <127314109+Stanfordrods1999@users.noreply.github.com>
2023-04-04 12:40:31 +0530

    Delete Grpahics directory

--- Grpahics/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test extends Linked_List
-{
-    Test()
-    {
-        Linked_List ob=new Linked_List();
-        ob.insert("Stanford","2018",20,0);
-        ob.insert("Sohail","2013",19,1);
-    }
-}
